19ebe9bc59bea8f1c05367726b7508d09bc42a9b
packo0 <pdimitrov1389@gmail.com>
2017-10-24 18:12:45 +0300

    solve first 7 tasks

---
+++ t02_SimpleCalculations/src/p01_SquareArea.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class p01_SquareArea {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int a = Integer.parseInt(scan.nextLine());
+
+        System.out.println(a * a);
+
+    }
+}
