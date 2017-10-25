fe0b8d1446f6f0083c1c194596d95e9b3d00a246
packo0 <pdimitrov1389@gmail.com>
2017-10-25 23:50:14 +0300

    solve some tasks

---
+++ t02_SimpleCalculations/src/p13_1000DaysAfterBirth.java
@@ -0,0 +1,14 @@
+import java.time.LocalDate;
+import java.util.Scanner;
+
+public class p13_1000DaysAfterBirth {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String birthDate = scan.nextLine();
+
+        LocalDate parsedBirthDate = LocalDate.parse(birthDate);
+        parsedBirthDate.plusDays(1000);
+
+        System.out.println(parsedBirthDate);
+    }
+}
