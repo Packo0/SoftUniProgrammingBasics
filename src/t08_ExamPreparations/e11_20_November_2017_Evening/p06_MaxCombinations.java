8cb28aea4553688e990f3344da827a3ac0749b1c
packo0 <pdimitrov1389@gmail.com>
2017-11-29 18:28:12 +0200

    solve task N 6

---
+++ src/t08_ExamPreparations/e11_20_November_2017_Evening/p06_MaxCombinations.java
@@ -0,0 +1,23 @@
+package t08_ExamPreparations.e11_20_November_2017_Evening;
+
+import java.util.Scanner;
+
+public class p06_MaxCombinations {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int begin = Integer.parseInt(scan.nextLine());
+        int end = Integer.parseInt(scan.nextLine());
+        int max = Integer.parseInt(scan.nextLine());
+
+        int count = 0;
+        for (int i = begin; i <= end; i++) {
+            for (int j = begin; j <= end; j++) {
+                count++;
+                System.out.printf("<%d-%d>", i, j);
+                if(count == max) {
+                    return;
+                }
+            }
+        }
+    }
+}
